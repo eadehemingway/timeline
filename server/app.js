f42496ce5f1f1c3941722e2bceb4b75857156346
eadehemingway <eade.hemingway@gmail.com>
2019-07-14 18:14:26 +0300

    add mode production, remove bodyparser

--- server/app.js
+++ server/app.js
@@ -4,11 +4,6 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../public/dist')));
