94b6da3e12ff539f1541fd36126d5a83ce02e091
eadehemingway <eade.hemingway@gmail.com>
2019-07-16 09:08:21 +0300

    tidy

--- public/components/App.js
+++ public/components/App.js
@@ -12,30 +12,29 @@ export class App extends React.Component {
         { id: 4, height: 30, liquidHeight: 20, color: '#FF851B' },
         { id: 5, height: 80, liquidHeight: 30, color: '#3D9970' },
         { id: 6, height: 20, liquidHeight: 5, color: '#9980FA' }
-      ]
+      ],
+      vaseY: 300
     };
   }
   componentDidMount() {
-    const { data } = this.state;
+    const { data, vaseY } = this.state;
     const svgWidth = 700;
     const svgHeight = 500;
-    const svg = d3
-      .select('#chart')
+    d3.select('#chart')
       .append('svg')
       .attr('width', svgWidth)
       .attr('height', svgHeight);
 
-    const vaseGroups = svg
+    const vaseGroups = d3
+      .select('svg')
       .selectAll('g')
       .data(data)
       .enter()
       .append('g')
-      .attr('class', 'vase');
-
+      .attr('class', 'vase-group');
     const margin = 230;
     const padding = 60;
     const vaseWidth = 15;
-    const vaseY = 300;
 
     vaseGroups
       .append('rect')
@@ -55,47 +54,59 @@ export class App extends React.Component {
       .attr('x', (_, i) => i * padding + margin)
       .attr('y', d => vaseY - d.liquidHeight)
       .attr('fill', d => d.color);
-
-    const updateLiquid = newData => {
-      vaseGroups.data(newData);
-      vaseGroups
-        .select('rect.liquid')
-        .transition()
-        .duration(750)
-        .attr('height', d => d.liquidHeight)
-        .attr('y', d => vaseY - d.liquidHeight);
-    };
-
-    ///////////////////////////////////////////////////////////////////////////
-
-    const increaseButton = document.querySelector('#increase');
-    increaseButton.addEventListener('click', () => {
-      const newData = data.map(d => {
-        const plusTen = d.liquidHeight + 10;
-        const newLiquidHeight = plusTen > d.height ? d.height : plusTen;
-        return { ...d, liquidHeight: newLiquidHeight };
-      });
-      this.setState({ data: newData });
-
-      updateLiquid(newData);
-    });
-
-    const decreaseButton = document.querySelector('#decrease');
-    decreaseButton.addEventListener('click', () => {
-      const newData = data.map(d => {
-        const minusTen = d.liquidHeight - 10;
-        const newLiquidHeight = minusTen > 0 ? minusTen : 0;
-        return { ...d, liquidHeight: newLiquidHeight };
-      });
-      this.setState({ data: newData });
-      updateLiquid(newData);
-    });
   }
+  componentDidUpdate() {
+    this.redrawLiquid(this.state.data);
+  }
+  redrawLiquid = newData => {
+    const { vaseY } = this.state;
+    const vaseGroups = d3.selectAll('.vase-group');
 
-  redraw = () => {
-    console.log('asdfasdf');
+    vaseGroups.data(newData);
+    vaseGroups
+      .select('rect.liquid')
+      .transition()
+      .duration(750)
+      .attr('height', d => d.liquidHeight)
+      .attr('y', d => vaseY - d.liquidHeight);
   };
+
+  updateData = direction => {
+    const { data } = this.state;
+
+    const newData = data.map(d => {
+      const newValue =
+        direction === 'increase' ? d.liquidHeight + 10 : d.liquidHeight - 10;
+
+      const newLiquidHeight =
+        newValue > d.height ? d.height : newValue < 0 ? 0 : newValue;
+
+      return { ...d, liquidHeight: newLiquidHeight };
+    });
+    this.setState({ data: newData });
+  };
+
   render() {
-    return <div id="chart" />;
+    return (
+      <section>
+        <div id="chart" />
+        <div className="button-container">
+          <button
+            id="increase"
+            className="increase-btn"
+            onClick={() => this.updateData('increase')}
+          >
+            +
+          </button>
+          <button
+            id="decrease"
+            className="decrease-btn"
+            onClick={() => this.updateData('decrease')}
+          >
+            -
+          </button>
+        </div>
+      </section>
+    );
   }
 }
